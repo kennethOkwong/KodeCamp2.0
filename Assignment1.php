24d1a389a0616a63afd856f9bc1d3bbe5ca7819a
kennethOkwong <okwongkenneth36@gmail.com>
2022-04-01 22:00:58 +0100

    Updated

--- Assignment1.php
+++ Assignment1.php
@@ -10,20 +10,20 @@ if (is_numeric($firstValue)){
     print_r("Enter the operator: \n");
     $operatorArray = ["+", "-", "*", "/"];
     $operator = readline();
-    if (in_array($operator, $operatorArray));
+    if (in_array($operator, $operatorArray)){
         print_r("Enter the second number: \n");
         $secondValue = readline();
         // Assignment Q1b - Mask input to accept numbers only (second input)
         if (is_numeric($secondValue)){
             switch ($operator){
                 case $operatorArray[0]:
-                    echo $firstValue + $secondValue;
+                    echo "ANS = " . $firstValue + $secondValue;
                     break;
                 case $operatorArray[1]:
-                    echo $firstValue - $secondValue;
+                    echo "ANS = " . $firstValue - $secondValue;
                     break;
                 case $operatorArray[2]:
-                    echo $firstValue * $secondValue;
+                    echo "ANS = " . $firstValue * $secondValue;
                     break;
                 // Assignment Q2 - Complete the code for division
                 case $operatorArray[3]:
@@ -32,11 +32,20 @@ if (is_numeric($firstValue)){
                         print_r("When divided, the result will be undefined");
                     }
                     else{
-                        echo $firstValue / $secondValue;
+                        echo "ANS = " . $firstValue / $secondValue;
                     }
                     break;
                     
+         
+            } 
+                
+        }
+        
+        else{
+            print_r("Invalid input\n");  
             }
+              
+    
     } 
     else{
     print_r("Invalid operator\n");  
