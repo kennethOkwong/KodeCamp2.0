aa5540037090b86268b6ab2502d7b0845ad84bf8
kennethOkwong <okwongkenneth36@gmail.com>
2022-04-19 12:41:40 +0100

    Create and Read todo functions updated

--- Promotion_task2/index.php
+++ Promotion_task2/index.php
@@ -5,6 +5,10 @@ require "todoHandler.php";
 $todo_1 = new Todo("todo.json");
 
 //Uncomment the code below to add todo as 
+$todo_1->AddTodo("Print Flex");
+$todo_1->AddTodo("Attend KodeCamp class");
 $todo_1->AddTodo("Remember to push your codes");
+
+$todo_1->ViewTodos();
 
 ?>
--- Promotion_task2/todoHandler.php
+++ Promotion_task2/todoHandler.php
@@ -9,6 +9,20 @@ class Todo extends JsonFileHandler{
         $this->SaveData($entry);
     }
 
+    public function ViewTodos(){
+        $todoList = $this->GetData();
+        echo "<b>ALL YOUR TODOs</b>";
+        echo "</br>";
+        $count = 1;
+        foreach ($todoList as $todos){
+            foreach ($todos as $key => $value){
+                echo $count . ". " . $value;
+                echo "</br>";
+                $count += 1;
+            }
+        }
+    }
+
 }
 
 ?>
